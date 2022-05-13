44a85a6bac94e54137fffd6b8ebe5502cac96e6c
SQT <samtruyen60@mail.com>
2022-05-13 22:25:45 +0700

    created errorFunctest class

---
+++ src/Model/DataModeTest/ErrorFunctionTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.DataModeTest;
+
+
+/**
+ *
+ * @author 21AK22
+ */
+public class ErrorFunctionTest {
+    private final String errorCode;
+    private final String errorDes;
+    private final String CusErrorCode;
+    private final String CusErrorDes;
+
+    
+    
+}
