f9185f7bebc859ca77ca91e95a862f0e4d1b68d5
SQT <samtruyen60@mail.com>
2023-09-11 13:44:17 +0700

    show item time in checktime, thay doi starttime thanh finish time trong up mydas

--- src/Control/Functions/FunctionsTest/Base/UpdateTime/CheckTimePC.java
+++ src/Control/Functions/FunctionsTest/Base/UpdateTime/CheckTimePC.java
@@ -10,7 +10,9 @@ import Control.Functions.FunctionsTest.Base.BaseFunction.FunctionBase;
 import Model.DataTest.FunctionParameters;
 import Time.TimeBase;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 /**
@@ -35,6 +37,7 @@ public class CheckTimePC extends AbsFunction {
     @Override
     protected boolean test() {
         try {
+            int spec = this.config.getInteger("delta_time", 900);
             TimeZone tz = TimeZone.getTimeZone(this.config.getString("timezone", "UTC"));
             String webUrl = this.config.getString("wedUrl", "http://time.windows.com");
             addLog(LOG_KEYS.CONFIG, "time zone: %s", tz.getDisplayName());
@@ -43,22 +46,25 @@ public class CheckTimePC extends AbsFunction {
             SimpleDateFormat sdf = new SimpleDateFormat(PatternDate);
             TimeZone.setDefault(tz);
             sdf.setTimeZone(tz);
-            String dateTime = sdf.format(this.timeBase.getWebsiteDatetime(webUrl));
+            Date wedDate = this.timeBase.getWebsiteDatetime(webUrl);
+            String dateTime = sdf.format(wedDate);
             try ( Cmd cmd = new Cmd()) {
-                this.functionBase.sendCommand(cmd, 
+                this.functionBase.sendCommand(cmd,
                         String.format("date %s", dateTime.split(" ")[0]));
                 cmd.readLine();
-                this.functionBase.sendCommand(cmd, 
+                this.functionBase.sendCommand(cmd,
                         String.format("time %s", dateTime.split(" ")[1]));
                 cmd.readLine();
             } catch (Exception e) {
             }
-
+            long wedms = wedDate.getTime();
+            long pctime = System.currentTimeMillis();
+            spec = spec * 1000;
             String timePc = getDateByFormatUTC(PatternDate);
-            String newTime = dateTime.substring(0, dateTime.lastIndexOf(":"));
-            addLog(LOG_KEYS.PC, "This time: %s", timePc);
-            addLog(LOG_KEYS.PC, "spec time: %s", newTime);
-            return timePc.contains(newTime);
+            addLog(LOG_KEYS.PC, "This time: %s - %s ms", timePc, pctime);
+            addLog(LOG_KEYS.PC, "spec time: %s - %s ms", dateTime, wedms);
+            addLog(LOG_KEYS.PC, "delta time: %s ms", spec);
+            return Math.abs(wedms - pctime) < spec;
         } catch (Exception ex) {
             ex.printStackTrace();
             addLog(LOG_KEYS.ERROR, ex.getLocalizedMessage());
